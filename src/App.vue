3918f6ad9a76bdcf2e7ea08347ea0808f7792919
sadovsky.aleksander@gmail.com <sadovsky.aleksander@gmail.com>
2020-08-12 01:49:35 +0200

    add a playlist and play/stop

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@
     </header>
     <main>
       <section class="player">
-        <h2 class="song-title">{{ current.title }}</h2>
+        <h2 class="song-title">{{ current.title }} - <span>{{ current. artist }}</span></h2>
+      <div class="contra">
+        <button class="prev">Prev</button>
+        <button class="play" v-if="!isPlaying" @click="play">Play</button>
+        <button class="pause" v-else @click="pause">Pause</button>
+        <button class="next">Next</button>
+      </div>
+      </section>
+      <section class="playlist">
+        <h3>The Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">{{ song.title }} - {{ song.artist }}</button>
       </section>
     </main>
   </div>
@@ -17,10 +27,49 @@ export default {
   name: 'App',
   data() {
     return {
-      current: {
-        title: 'SONG TITLE'
-      }
+      current: {},
+      index: 0,
+      isPlaying:false,
+      songs: [
+        {
+          title: 'Miles Powers',
+          artist: 'Back To Business(feat. Laundry, Kid April & Dead Chemist)',
+          src: require('./assets/Back-To-Business.mp3')
+        },
+        {
+          title: 'Miles Powers',
+          artist: 'I take A Pill (feat.Dahm)',
+          src: require('./assets/Back-To-Business.mp3')
+        },
+        {
+          title: 'strange day',
+          artist: 'Disco',
+          src: require('./assets/Disco.mp3')
+        },
+      ],
+      player: new Audio()
     }
+  },
+  methods: {
+    play(song) {
+      if (typeof song.src != "undefined") {
+        this.current = song;
+
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause() {
+      this.player.pause();
+      this.isPlaying = false;
+    }
+  },
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    this.player.play();
   }
 }
 </script>
@@ -34,7 +83,7 @@ export default {
 }
 
 body { 
-  font-family: 'Times New Roman', Times, serif;
+  font-family:Arial, Helvetica, sans-serif
 }
 
 header {
