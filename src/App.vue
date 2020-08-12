a1bd583aa62777247b17d541e23cec463d2e0f2e
sadovsky.aleksander@gmail.com <sadovsky.aleksander@gmail.com>
2020-08-12 15:25:23 +0200

    basic styling

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,18 @@
     <main>
       <section class="player">
         <h2 class="song-title">{{ current.title }} - <span>{{ current. artist }}</span></h2>
-      <div class="contra">
-        <button class="prev">Prev</button>
+      <div class="controls">
+        <button class="prev" @click="prev">Prev</button>
         <button class="play" v-if="!isPlaying" @click="play">Play</button>
         <button class="pause" v-else @click="pause">Pause</button>
-        <button class="next">Next</button>
+        <button class="next" @click="next">Next</button>
       </div>
       </section>
       <section class="playlist">
         <h3>The Playlist</h3>
-        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">{{ song.title }} - {{ song.artist }}</button>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+          {{ song.title }} - {{ song.artist }}
+        </button>
       </section>
     </main>
   </div>
@@ -29,7 +31,7 @@ export default {
     return {
       current: {},
       index: 0,
-      isPlaying:false,
+      isPlaying: false,
       songs: [
         {
           title: 'Miles Powers',
@@ -39,7 +41,7 @@ export default {
         {
           title: 'Miles Powers',
           artist: 'I take A Pill (feat.Dahm)',
-          src: require('./assets/Back-To-Business.mp3')
+          src: require('./assets/I-Take-A-Pill.mp3')
         },
         {
           title: 'strange day',
@@ -59,11 +61,41 @@ export default {
       }
 
       this.player.play();
+      this.player.addEventListener('ended', function () {
+        this.index++
+      if (this.index > this.songs.length - 1) {
+        this.index = 0
+      }
+
+      this.current = this.songs[this.index]
+      this.play(this.current)
+      }.bind(this))
+
       this.isPlaying = true;
     },
     pause() {
       this.player.pause();
       this.isPlaying = false;
+    },
+    next () {
+      this.index++;
+      if (this.index > this.songs.length - 1) {
+        this.index = 0
+      }
+
+      this.current = this.songs[this.index]
+
+      this.play(this.current)
+    },
+    prev() {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.songs.length -1
+      }
+
+      this.current = this.songs[this.index]
+
+      this.play(this.current)
     }
   },
   created() {
@@ -76,23 +108,95 @@ export default {
 
 <style>
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
-
-body { 
-  font-family:Arial, Helvetica, sans-serif
+body {
+	font-family: sans-serif;
 }
-
 header {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 15px;
+	background-color: #212121;
+	color: #FFF;
+}
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title {
+  color: #53565A;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+.controls {
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 30px 15px;
+}
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+.play, .pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: #FFF;
+  background-color: #395dff;
+}
+.next, .prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #FFF;
+  background-color: #395dff;
+}
+.playlist {
+  padding: 0px 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song {
+  display: block;
+  width: 100%;
   padding: 15px;
-  background-color: #212121;
-  color: #fff
-
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover {
+  color: #395dff;
+}
+.playlist .song.playing {
+  color: #FFF;
+  background-image: linear-gradient(to right, #0e31f3, #64bbf5);
 }
 </style>
